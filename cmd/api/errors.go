f264c0575dcfe395dcb1912c963c05291f92ae72
elena4ernova <eachernova@iifmail.ru>
2026-02-02 14:11:19 +0300

    fix: race condition if two clients try to update the movie

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -60,3 +60,10 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+// editConflictResponse - метод используется для отправки клиенту 409.
+// Возникает в случае конфликта при записи данных измененных.
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
