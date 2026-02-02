2ee4923291d6cd9d220fb51bdbb0e6d153182ea1
elena4ernova <eachernova@iifmail.ru>
2026-02-02 10:58:37 +0300

    add (R): handler get movie from DB

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/len4ernova/lets_go_further/internal/data"
 	"github.com/len4ernova/lets_go_further/internal/validator"
@@ -81,19 +81,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		//		http.NotFound(w, r)
 		return
 	}
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casabl",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
-		// app.logger.Error(err.Error())
-		// http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
+
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/len4ernova/lets_go_further/internal/validator"
@@ -44,7 +45,7 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 
 }
 
-// placeholder - метод создающий запись в БД.
+// метод создающий запись в БД.
 func (m MovieModel) Insert(movie *Movie) error {
 	query := `
 		INSERT INTO movies (title, year, runtime, genres)
@@ -57,7 +58,35 @@ func (m MovieModel) Insert(movie *Movie) error {
 
 // placeholder - метод получение конкретной записи из БД.
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	query := `
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		WHERE id = $1;	`
+
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
 }
 
 // placeholder - метод обновляет конкретную запись в БД.
