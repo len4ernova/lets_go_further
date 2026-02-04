729d91efe68a2756ed3412d8f8dff5ea32052800
chernish <len4ernova@yandex.ru>
2026-02-04 22:34:55 +0300

    step 2: rate limiting (ip-users map) & flags for limit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -27,6 +27,13 @@ type config struct {
 		maxIdleConns int           // PostgreSQL макс неактивных соединений
 		maxIdleTime  time.Duration // PostgreSQL продолжительность неакт. соединения
 	}
+	//  структура-ограничитель, содержит поля для значений количества запросов в секунду и пиковой нагрузки,
+	// а также логическое поле, с помощью которх можем включать и выключать ограничение скорости
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 // an application struct to hold the dependencies for our HTTP handlers, helpers, and middleware
@@ -49,6 +56,10 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
 	flag.Parse()
 
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -27,15 +30,66 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 // rateLimit - ограничитель запросов. корзина с токенами пополняется со скоростью два токена в секунду.
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// инициализируем новый ограничитель запросов в среднем 2 запроса/сек, макс 4
-	limiter := rate.NewLimiter(2, 4)
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Вызываем метод limiter.Allow(), чтобы проверить, разрешен ли запрос.
-		// Если нет, то мы вызываем вспомогательный метод rateLimitExceededResponse()
-		if !limiter.Allow() {
-			app.rateLimitExceededResponse(w, r)
-			return
+
+	// клиентская структуру для хранения ограничителя скорости и времени последнего посещения для каждого клиента
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	// мьютекс и карт для хранения IP-адресов клиентов и ограничителей скорости
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+	// фоновая горутина, раз в минуту удаляет старые записи с карты клиентов.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			// Заблокируйте мьютекс, чтобы предотвратить выполнение любых проверок ограничителя скорости во время очистки.
+			mu.Lock()
+			// Перебираем всех клиентов.
+			// Если их не было видно в течение последних трех минут, удаляем соответствующую запись с карты
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			//  разблокировать мьютекс после завершения очистки
+			mu.Unlock()
 		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// проверка выполняется если включен соотв-ий флаг
+		if app.config.limiter.enabled {
+			// получить IP-адрес клиента
+			ip := realip.FromRequest(r)
+			// Заблокируйте мьютекс, чтобы этот код не выполнялся одновременно с другими
+			mu.Lock()
+			// Проверьте, есть ли этот IP-адрес на карте. Если нет, то
+			// инициализируйте новый ограничитель скорости и добавьте IP-адрес и ограничитель на карту
+			if _, found := clients[ip]; !found {
+				// Создайте и добавьте на карту новую структуру клиента, если она еще не существует.
+				// инициализируем новый ограничитель запросов в среднем 2 запроса/сек, макс 4
+				clients[ip] = &client{
+					// Используйте значения количества запросов в секунду и пиковой нагрузки из конфигурации структуры
+					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+				}
+			}
+			// Обновите время последнего посещения клиента.
+			clients[ip].lastSeen = time.Now()
+
+			// метод Allow() для ограничителя скорости текущего IP-адреса.
+			// Если запрос не разрешен, разблокируйте мьютекс и отправьте ответ с кодом 429
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+			// разблокировать мьютекс перед вызовом следующего обработчика в цепочке.
+			mu.Unlock()
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
